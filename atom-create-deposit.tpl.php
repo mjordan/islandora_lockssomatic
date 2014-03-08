350fc441e62b345c2e47e3972b254d79215afcb5
Mark Jordan <mjordan@sfu.ca>
2014-03-08 09:26:35 -0800

    Added a GMT/UTC timestamp to the Atom XML.

--- atom-create-deposit.tpl.php
+++ atom-create-deposit.tpl.php
@@ -6,6 +6,10 @@
  * Available variables:
  *   $au_title string
  *     The AU's title.
+ *   $uuid string
+ *     A UUID.
+ *   $utc_timestamp
+ *     A GMT/UTC timestamp.
  *   $site_name string
  *     The value of variable_get('site_name', '').
  *   $entries array
@@ -17,7 +21,7 @@
         xmlns:lom="http://lockssomatic.info/SWORD2">
   <title><?php print $au_title; ?></title>
   <id>urn:uuid:<?php print $uuid; ?></id>
-  <updated>2013-10-07T17:17:08Z</updated>
+  <updated><?php print $utc_timestamp; ?></updated>
   <summary type="text">Content deposited to LOCKSS-O-Matic by content provider <?php print $site_name; ?>.</summary>
   <?php foreach ($entries as $entry) : ?>
   <lom:content size="<?php print $entry['size']; ?>" checksumType="<?php print $entry['checksum_type']; ?>" checksumValue="<?php print $entry['checksum_value']; ?>"><?php print $entry['url']; ?></lom:content>
